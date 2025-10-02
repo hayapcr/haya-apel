a0cadcb4b46cb3a9dc8018105c22acb9b0733070
hayapcr <haya24si@mahasiswa.pcr.ac.id>
2025-10-02 10:50:10 +0700

    Membuat views dan controller serta menambahkan route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -139,7 +139,7 @@
                         </div>
                         <ul class="list-group mb-3">
                             @foreach ($list_pendidikan as $item)
-                                <li class="list-group-item">{{ $item }}</li>
+                                <li class = "list-group-item"> {{$item}}</li>
                             @endforeach
                         </ul>
                         <div class="p-3 border rounded">
@@ -153,6 +153,40 @@
             </div>
 
             <div class="col-md-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Pertanyaan</h5>
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form action="{{ route('question.store') }}" method="POST">
+                    @csrf
+
+                    <form action="" method="POST">
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama</label>
+                            <input type="text" class="form-control" name="nama" value="{{ old('nama') }}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" name="email" value="{{ old('email') }}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pertanyaan" class="form-label">Pertanyaan</label>
+                            <textarea class="form-control" rows="4">{{ old('pertanyaan') }}</textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Kirim Pertanyaan</button>
+                    </form>
+                </div>
+            </div>
                 {{-- Alerts --}}
                 <div class="card ">
                     <div class="card-body">
