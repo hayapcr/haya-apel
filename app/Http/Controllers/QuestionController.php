872ab809e620ac0815acc71d45c33038207c36fc
hayapcr <haya24si@mahasiswa.pcr.ac.id>
2025-10-09 10:19:32 +0700

    :Pembuatan Halaman Dashboard

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -40,7 +40,10 @@ class QuestionController extends Controller
         $data['email']      = $request->email;
         $data['pertanyaan'] = $request->pertanyaan;
 
-        return view('home-question-respon', $data);
+        //return view('home-question-respon', $data);
+        return redirect()->route('home')->with('info', 'Terimakasih <b>'.$data['nama'].
+        '</b> Data berhasil di inputkan, Silahkan cek email <b>'
+        . $data['email']. '</b>');
 
     }
 
