b452f20f013619904e448784c2e8d725ca364d90
hayapcr <haya24si@mahasiswa.pcr.ac.id>
2025-11-27 15:37:15 +0700

    Perubahan yg salah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             CreateFirstUser::class,
-            CreatePelangaanDummy::class,
+            CreatePelangganDummy::class,
         ]);
     }
 }
